ea3689ae55bf2d58422f659014896d056ca19d8e
cuijie <yinsu@foxmail.com>
2016-07-25 17:12:17 +0800

    promise

---
+++ .vscode/promise1.js
@@ -0,0 +1,16 @@
+fs.readFile('./package.json', (err, data) => {
+    if(err) throw err;
+    console.log(data.toString());
+})
+
+function getFile(file) {
+    return new Promise(function(resolve, reject){
+        fs.readFile(file, (err, data) => {
+            if(err){
+                reject(err);
+            }else{
+                resolve(data.toString());
+            }
+        })
+    });
+}
---
+++ class3.js
@@ -0,0 +1,71 @@
+//类工厂
+function filteredSetSubclass(superclass, filter) {
+    var consturctor = function () {
+        superclass.apply(this, arguments);
+    }
+
+    var proto = constructor.prototype = Object.create(superclass.prototype);
+    proto.consturctor = constructor;
+
+    proto.add = function () {
+        for (var i = 0, len = arguments.length; i < len; i++) {
+            if (!filter(arguments[i])) {
+                throw 'error';
+            }
+        }
+
+        superclass.prototype.add.apply(this, arguments);
+    }
+
+    return constructor;
+}
+
+//组合
+var FilterSet = Set.extend(
+    function FilteredSet(set, filter) {
+        this.set = set;
+        this.filter = filter;
+    },
+    {
+        add: function () {
+            if (this.filter) {
+                for (var i = 0, len = arguments.length; i < len; i++) {
+                    if (!this.filter(arguments[i])) {
+                        throw "filter rejected";
+                    }
+                }
+            }
+
+            this.set.add.apply(this, arguments);
+            return this;
+        },
+        remove: function () {
+            this.set.remove.apply(this, arguments);
+            return this;
+        },
+        constains: function (v) {
+            return this.set.constains(v);
+        }
+    }
+)
+
+//接口
+function abstractmethod() { throw new Error("abstract method"); }
+
+//类，包含一个抽象方法
+function AbstractSet() { throw new Error("Can't instantiate abstract classes"); }
+AbstractSet.prototype.constians = abstract;
+
+//继承了AbstractSet类，并实现了上面的抽象方法
+var NotSet = AbstractSet.extend(
+    function NotSet(set) { this.set = set; },
+    {
+        constains: function (x) { return !this.set.constains(x); },
+        toString: function () { return "~" + this.set.toString(); }
+    }
+)
+
+//模块
+var sets = {};
+sets.SingletonSet = sets.AbstractSet.extend();
+
