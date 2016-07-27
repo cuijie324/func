33f1c8be5b277012d2d2d84faf29ae902c459000
cuijie <yinsu@foxmail.com>
2016-07-27 18:52:14 +0800

    async

--- promise1.js
+++ promise1.js
@@ -1,12 +1,13 @@
 var fs = require('fs');
 
-fs.readFile('./package.json', (err, data) => {
-    if(err) throw err;
-    console.log(data.toString());
-})
+// fs.readFile('./package.json', (err, data) => {
+//     if(err) throw err;
+//     console.log(data.toString());
+// })
 
 function getFile(file) {
     return new Promise(function(resolve, reject){
+        console.log("promise is starting...");
         fs.readFile(file, (err, data) => {
             if(err){
                 reject(err);
@@ -16,3 +17,39 @@ function getFile(file) {
         })
     });
 }
+
+console.log("start");
+
+getFile('./package.json').then(function(data){
+    console.log("收到数据...");
+    return JSON.parse(data);
+}).then(data => {
+    console.log(data);
+    return data.name
+}).then(data => {
+    console.log(data);
+})
+.catch(err => {
+    console.log("catch", err);
+})
+
+console.log("end");
+
+console.log("start2");
+
+getFile('./package.json').then(function(data){
+    console.log("收到数据2...");
+    return JSON.parse(data);
+}).then(data => {
+    return new Promise(function(resolve, reject){
+        console.log(data);
+        resolve(data.name);
+    })
+}).then(data => {
+    console.log(data);
+})
+.catch(err => {
+    console.log("catch", err);
+})
+
+console.log("end2");
