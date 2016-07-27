171714690858e510a05d66b32b7b7f3c84c56dd0
cuijie <yinsu@foxmail.com>
2016-07-27 16:08:18 +0800

    test

--- object1.js
+++ object1.js
@@ -1,3 +1,43 @@
-var obj = { a: "11", b: "22", c: "33" };
+var obj = { a: "1", b: "2", c: "3" };
 
-console.log(Object.isExtensible(obj), Object.isSealed(obj));
+console.log("\nstarting...");
+testObj();
+obj.d = "4";
+console.log(obj);
+//console.log(Object.getOwnPropertyDescriptor(obj, 'a'));
+Object.defineProperty(obj, "aa", {value: '11', writable: true, enumerable: true, configurable: true});
+console.log(obj);
+
+console.log("\npreventExtensions...");
+Object.preventExtensions(obj);
+testObj();
+obj.e = "5";
+console.log(obj);
+delete obj.d;
+console.log(obj);
+obj.a = "11";
+Object.defineProperty(obj, "bb", {value: '22', writable: true, enumerable: true, configurable: true});
+console.log(obj);
+
+console.log("\nseal...");
+Object.seal(obj);
+testObj();
+console.log(obj);
+delete obj.d;
+console.log(obj);
+obj.a = '111';
+console.log(obj);
+
+console.log("\nfreeze...");
+Object.freeze(obj);
+testObj();
+console.log(obj);
+obj.a = '1111';
+console.log(obj);
+
+
+
+//锁定 封闭 冻结
+function testObj(){
+    console.log("isExtensible: " + Object.isExtensible(obj) + " isSealed: " + Object.isSealed(obj) + " isFrozen: " + Object.isFrozen(obj));
+}
--- promise1.js
+++ promise1.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+
 fs.readFile('./package.json', (err, data) => {
     if(err) throw err;
     console.log(data.toString());
