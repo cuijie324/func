1452f1d0f9edd89231d8c2b8430d14506cd51dac
cuijie <yinsu@foxmail.com>
2016-07-30 12:59:17 +0800

    自动执行thunk形式的函数

--- async2.js
+++ async2.js
@@ -1,6 +1,7 @@
 // thunk函数学习
 
 var fs = require('fs');
+var thunkify = require('thunkify');
 
 // 原先的使用方式
 //fs.readFile(fileName, callback);
@@ -8,13 +9,62 @@ var fs = require('fs');
 // readFileThunk(callback);
 
 //封装成thunk形式
-var readFile = function (fileName) {
-    return function (callback) {
-        return fs.readFile(fileName, callback);
-    }
+// var readFile = function (fileName) {
+//     return function (callback) {
+//         return fs.readFile(fileName, callback);
+//     }
+// }
+
+var readFile = thunkify(fs.readFile);
+// readFile('a.text')((err, data) => {
+//     console.log(data.toString());
+// })
+
+var gen = function* (){
+    var r1 = yield readFile('a.text');
+    console.log(r1.toString());
+    var r2 = yield readFile('b.text');
+    console.log(r2.toString());
 }
 
-readFile('a.text')((err, data) => {
-    console.log(data.toString());
-})
+//手动执行上面的generator函数
+function gen_exec(){
+    var g = gen();
+    var r1 = g.next();
+    r1.value(function(err, data){
+        if(err) throw err;
+        var r2 = g.next(data);
+        r2.value(function(err, data){
+            if(err) throw err;
+            g.next(data);
+        })
+    })
+}
+//gen_exec();
+
+//自动执行
+function gen_auto(fn){
+    var gen = fn();
+
+    // function next(data){
+    //     var result = gen.next(data);
+    //     if(result.done) return;
+    //     result.value(function(err, data){
+    //         if(err) throw err;
+    //         //gen.next(data);
+    //         next(data);
+    //     })
+    // }
+    function next(err, data){
+        var result = gen.next(data);
+        if(result.done) return;
+        result.value(next);
+    }
+
+    next();
+}
+
+gen_auto(gen);
+
+
 
